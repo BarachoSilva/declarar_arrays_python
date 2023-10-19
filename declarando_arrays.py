17c9349bfed0b04545dff38b0fe3b04769d85a62
Alex Baracho <alex_rodrigues_11@hotmail.com>
2023-10-19 05:17:52 -0300

    declarando_arrays

---
+++ declarando_arrays.py
@@ -0,0 +1,9 @@
+# Array de produtos de uma loja de comida
+produtos_comida = ["arroz", "feijão", "macarrão", "carne", "legumes"]
+
+# Array de anos de nascimento de familiares e amigos
+anos_nascimento = [1990, 1985, 1992, 1980, 2000]
+
+# Imprimindo os arrays no terminal
+print("Produtos de uma loja de comida:", produtos_comida)
+print("Anos de nascimento de familiares e amigos:", anos_nascimento)
